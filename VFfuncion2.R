894605f5d05137350e80a673d2f427384bb9930f
CesarAyCD <144299294+CesarAyCD@users.noreply.github.com>
2023-09-06 20:44:40 -0600

    Add files via upload

---
+++ VFfuncion2.R
@@ -0,0 +1,18 @@
+VFfuncion2(0.00000001, 100000)
+
+VFfuncion2 = function(umbral, nIter){
+  i=0
+  VF0=0
+  DeltaVF=Inf
+  while(DeltaVF > umbral & i < nIter){
+    VF=(1+(1/i))^i
+    DeltaVF=VF-VF0
+    VF0=VF
+    i=i+1
+  }
+  if(i >= nIter){
+    print(paste("Se llegó al valor ",VF0," con ",i," iteraciones"))
+  }else{
+    print(paste("Se llegó al valor ",VF0," con el umbral ",umbral))
+  }
+}
